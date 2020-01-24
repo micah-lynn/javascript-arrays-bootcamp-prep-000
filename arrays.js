5c5b231c5a972dfac9155af82f3a8536fe1e9406
Micah Lynn <micah@sabado.com>
2020-01-24 18:36:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,5 +33,6 @@ function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {
 }
 
 function removeElementFromBeginningOfArray (chocolateBars) {
-  
+  chocolateBars.slice(1);
+  return chocolateBars;
 }
